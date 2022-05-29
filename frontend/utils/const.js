f8a76795192d443433f4bb0e68fdc282b792f54e
web3dev26 <web3.dev26@hotmail.com>
2022-05-29 20:12:13 +0300

    final

--- frontend/utils/const.js
+++ frontend/utils/const.js
@@ -3,17 +3,19 @@ import tiktok from './tiktok_clone.json';
 
 export const CLUSTER =
   process.env.REACT_APP_CLUSTER === "devnet"
-    ? "devnet";
+    ? "devnet"
+    : '';
 
 export const SOLANA_HOST = process.env.REACT_APP_SOLANA_API_URL
   ? process.env.REACT_APP_SOLANA_API_URL
   : CLUSTER === "devnet"
-    ? clusterApiUrl("devnet");
+    ? clusterApiUrl("devnet")
+    : '' ;
 
 export const TIKTOK_PROGRAM_ID = new PublicKey(
   CLUSTER === 'devnet' 
   ? '84ycHNkVrLem741fXUGNTyufi2qNGMbLVcwkZDZmyNwb' 
-  
+  : ''
 );
 
 export const TIKTOK_IDL = tiktok;
